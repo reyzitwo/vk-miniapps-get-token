40ee28444b3446a15bb87115efdc72ce2e3f1e7e
Aleksandr Kuznetsov <sahakuz03@gmail.com>
2024-03-27 00:16:06 +0500

    refactoring

--- index.js
+++ index.js
@@ -1,91 +1,106 @@
-const webdriver = require("selenium-webdriver");
+require("chromedriver");
+const { Builder, By } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
-const path = require("path");
 
-const client_id = 6831669;
-const login = ""; // логин аккаунта
-const password = ""; // пароль
-
-const url = `https://oauth.vk.com/authorize?client_id=${client_id}&scope=228573151&redirect_uri=close.html&display=page&response_type=token&revoke=1`; // ссылка на получение токена
+const account = { login: "", password: "" };
 
 const chromeOptions = new chrome.Options();
+chromeOptions.addArguments("--remote-debugging-port=9222");
 chromeOptions.addArguments("--headless");
 chromeOptions.addArguments("--no-sandbox");
 
-const service = new chrome.ServiceBuilder(
-  path.join(__dirname, "./chromedriver")
-);
-
-const driver = new webdriver.Builder()
+const driver = new Builder()
   .forBrowser("chrome")
-  .setChromeService(service)
   .setChromeOptions(chromeOptions)
-  .build(); // создаем вебдрайвер
+  .build();
 
-(async function () {
-  try {
-    await driver.get("https://vk.com/"); // открываем vk.com
+let isLogin = false;
+login();
 
-    await driver
-      .findElement(
-        webdriver.By.xpath(
-          '//button[@class="FlatButton FlatButton--primary FlatButton--size-l FlatButton--wide VkIdForm__button VkIdForm__signInButton"]'
-        )
-      )
-      .click(); // переходим на страницу авторизации
-    await sleep(4000); // ждем пока вк прогрузит страницу
+async function login() {
+  await driver.get("https://vk.com/"); // открываем vk.com
 
-    await driver
-      .findElement(webdriver.By.xpath('//input[@name="login"]'))
-      .sendKeys(login); // вводим логин
-    await driver
-      .findElement(webdriver.By.xpath('//button[@type="submit"]'))
-      .click(); // переходим далее
-    await sleep(4000);
+  (await lazyDriver())
+    .findElement(
+      By.xpath(
+        '//button[@class="FlatButton FlatButton--primary FlatButton--size-l FlatButton--wide VkIdForm__button VkIdForm__signInButton"]',
+      ),
+    )
+    .click();
 
-    await driver
-      .findElement(webdriver.By.xpath('//input[@name="password"]'))
-      .sendKeys(password); // вводим пароль
-    await driver
-      .findElement(webdriver.By.xpath('//button[@type="submit"]'))
-      .click(); // авторизуемся
-    await sleep(2000);
+  (await lazyDriver())
+    .findElement(By.xpath('//input[@name="login"]'))
+    .sendKeys(account.login);
 
-    await driver.get(url); // получаем страницу для получения токена
-    await driver
-      .findElement(
-        webdriver.By.xpath('//button[@class="flat_button fl_r button_indent"]')
-      )
-      .click(); // соглашаемся на получение токена
+  (await lazyDriver())
+    .findElement(By.xpath('//button[@type="submit"]'))
+    .click();
 
-    const response = parseOauthVK(await driver.getCurrentUrl());
+  (await lazyDriver())
+    .findElement(
+      By.xpath('//button[@data-test-id="other-verification-methods"]'),
+    )
+    .click();
 
-    console.log(response);
-  } finally {
-    await driver.quit(); // закрываем вебдрайвер
+  (await lazyDriver())
+    .findElement(By.xpath('//div[@data-test-id="verificationMethod_password"]'))
+    .click();
+
+  (await lazyDriver())
+    .findElement(By.xpath('//input[@name="password"]'))
+    .sendKeys(account.password);
+
+  (await lazyDriver())
+    .findElement(By.xpath('//button[@type="submit"]'))
+    .click();
+
+  isLogin = true;
+}
+
+async function getToken(client_id) {
+  if (!isLogin) {
+    console.log(
+      `[${getTimestamp()}] The process of logging into an account via selenuim...`,
+    );
+
+    return setTimeout(() => getToken(client_id), 5 * 1000);
   }
-})();
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  (await lazyDriver()).get(
+    `https://oauth.vk.com/authorize?client_id=${client_id}&scope=228573151&redirect_uri=close.html&display=page&response_type=token&revoke=1`,
+  );
+  await driver
+    .findElement(By.xpath('//button[@class="flat_button fl_r button_indent"]'))
+    .click();
+
+  const response = parseOauthVK(await driver.getCurrentUrl());
+  console.log(response);
+}
+
+function lazyDriver() {
+  return new Promise((res) => setTimeout(() => res(driver), 4000));
+}
+
+getToken(6831669);
+
+function getTimestamp() {
+  return Math.floor(new Date().getTime() / 1000);
 }
 
 function parseOauthVK(url) {
-  const paramsString = url.split("#")[1]; // Получаем строку параметров после символа '#'
-  const paramsArray = paramsString.split("&"); // Разбиваем строку на массив параметров
+  const paramsString = url.split("#")[1];
+  const paramsArray = paramsString.split("&");
 
-  // Создаем пустой объект, в который будем добавлять параметры
   const params = {};
 
-  // Проходимся по каждому параметру и добавляем его в объект params
   paramsArray.forEach((param) => {
-    const [key, value] = param.split("="); // Разбиваем параметр на ключ и значение
-    params[key] = value; // Добавляем параметр в объект params
+    const [key, value] = param.split("=");
+    params[key] = value;
   });
 
   return {
     ...params,
-    expires_in: Number(params.expires_in),
+    expires_in: getTimestamp() + Number(params.expires_in),
     user_id: Number(params.user_id),
   };
 }
